96b0750af9f6f83190aea37259c19c9be6934767
Pozz Tong <pozztong12800900@gmail.com>
2024-10-16 23:15:57 +0700

    push

---
+++ book_flips/example/book_flips_example.dart
@@ -0,0 +1,6 @@
+import 'package:book_flips/book_flips.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ book_flips/test/book_flips_test.dart
@@ -0,0 +1,16 @@
+import 'package:book_flips/book_flips.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
